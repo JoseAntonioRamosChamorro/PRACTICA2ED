ce06161e55151c60cdde0cc70e26eda22632c10f
JoseAnt <JoseAnt@DESKTOP-B4QHI3K.localdomain>
2020-01-04 23:56:22 +0100

    #3 Comentarios adicionales
    Se crearon comentarios adicionales mediante el metodo de javadoc y se
    creo el propio documento javadoc para recopilarlo

--- src/practica2/TestBanco.java
+++ src/practica2/TestBanco.java
@@ -1,7 +1,15 @@
 package practica2;
-
+/**
+ *  
+ * @author JoseAnt
+ * @version 1.0
+ */
 public class TestBanco {
+
 	public static void main(String[] args) {
+		/**
+		 * 
+		 */
 		/* Antonio y Beatriz se hacen cliente del banco */
 		Cliente antonio = new Cliente("123456789Z", "Antonio Alonso", "Av. Pueblo Saharaui, s/n");
 		Cliente beatriz = new Cliente("987654321A", "Beatriz Benítez", "Calle Sol, 4");
@@ -30,35 +38,58 @@ public class TestBanco {
 	}
 
 	private static void SaldoInicial(Cuenta cuentaAntonio, Cuenta cuentaBeatriz) {
+		
 		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "
 				+ cuentaAntonio.getSaldo() + " euros.");
 		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "
 				+ cuentaBeatriz.getSaldo() + " euros.");
 	}
-
+/**
+ * 
+ * @param cuentaAntonio contiene la informacion de la cuenta de de Antonio
+ * @param cuentaBeatriz contiene la informacion de la cuenta de de Beatriz
+ */
 	private static void TransferenciaAntonio_Beatriz(Cuenta cuentaAntonio, Cuenta cuentaBeatriz) {
 		int transferencia = 50;
 		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() - transferencia);
 		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() + transferencia);
 	}
-
+/**
+ * 
+ * @param cuentaBeatriz contiene la informacion de la cuenta de de Beatriz
+ * 
+ */
 	private static void PagarHacienda(Cuenta cuentaBeatriz) {
+
 		int pago = 30;
 		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() - pago);
 	}
 
+/**
+ * 
+ * @param cuentaAntonio contiene la informacion de la cuenta de de Antonio
+ */
 	private static void Rifa(Cuenta cuentaAntonio) {
 		int conseguidoenlaloteria = 100;
 		cuentaAntonio.setSaldo(cuentaAntonio.getSaldo() + conseguidoenlaloteria);
 	}
 
+	/**
+	 * 
+	 * @param cuentaAntonio contiene la informacion de la cuenta de de Antonio
+	 * @param cuentaBeatriz contiene la informacion de la cuenta de de Beatriz
+	 */
 	private static void ConsultarSaldoActual(Cuenta cuentaAntonio, Cuenta cuentaBeatriz) {
 		System.out.println("La cuenta de " + cuentaAntonio.getCliente().getNombre() + " tiene "
 				+ cuentaAntonio.getSaldo() + " euros.");
 		System.out.println("La cuenta de " + cuentaBeatriz.getCliente().getNombre() + " tiene "
 				+ cuentaBeatriz.getSaldo() + " euros.");
 	}
-
+/**
+ * 
+ * @param cuentaAntonio contiene la informacion de la cuenta de de Antonio
+ * @param cuentaBeatriz contiene la informacion de la cuenta de de Beatriz
+ */
 	private static void PrimeraTransferencia(Cuenta cuentaAntonio, Cuenta cuentaBeatriz) {
 		int transferir = 50;
 		cuentaBeatriz.setSaldo(cuentaBeatriz.getSaldo() - transferir);
